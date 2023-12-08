92306a40551e71151e5b0ffeaa97a1f3a57cd6cd
Matheus Barboza <matheus.barboza102@gmail.com>
2023-12-08 19:32:56 -0300

    style: refactor vehicleModal styles

--- src/components/ui/vehicleModal.tsx
+++ src/components/ui/vehicleModal.tsx
@@ -17,19 +17,33 @@ export function VehicleModal({ isOpen, onClose, vehicle }: ModalProps) {
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="sm:max-w-[425px] bg-white rounded-lg border-transparent">
         <DialogHeader className="flex justify-between items-start">
-          <DialogTitle className="flex gap-3">
-            <span>{vehicle?.plate}</span>
+          <DialogTitle className="flex items-center gap-3">
+            <span className="text-zinc-400">{vehicle?.plate}</span>
             <span>|</span>
-            <span>{vehicle?.status}</span>
+            <div
+              className={`p-2 rounded-xl text-white bg-gradient-to-r ${
+                vehicle?.status.toLowerCase() === 'agendado' ||
+                vehicle?.status.toLowerCase() === 'reagendado' ||
+                vehicle?.status.toLowerCase() === 'aprovado'
+                  ? 'from-green-300 to-green-700'
+                  : vehicle?.status.toLowerCase() === 'pendente'
+                    ? 'from-orange-300 to-orange-500'
+                    : vehicle?.status.toLowerCase() === 'reprovado'
+                      ? 'from-pink-700 to-red-700'
+                      : ''
+              }`}
+            >
+              {vehicle?.status}
+            </div>
           </DialogTitle>
         </DialogHeader>
 
         <div className="mt-5 font-semibold">{vehicle?.clientName}</div>
         <div className="flex items-center justify-start gap-5 mt-6">
-          <div className="bg-gradient-to-r from-orange-300 to-orange-400 p-2 rounded-xl text-white">
+          <div className="border border-zinc-600 p-2 rounded-xl text-zinc-600">
             {vehicle?.technology}
           </div>
-          <div className="bg-gradient-to-r from-yellow-300 to-yellow-400 p-2 rounded-xl text-white">
+          <div className="border border-zinc-600 p-2 rounded-xl text-zinc-600">
             {vehicle?.linkCategory}
           </div>
         </div>
